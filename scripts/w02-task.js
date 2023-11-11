aa129e013a6bf83e2d391bd8396dd11b4d3ffd0e
etharas130 <pet19020@byui.edu>
2023-11-11 02:08:02 -0700

    All steps done: first test

---
+++ scripts/w02-task.js
@@ -0,0 +1,52 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Brenin Peterson';
+
+let currentYear = new Date().getFullYear();
+
+let profilePicture = 'images/brenindpeterson.jpg';
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+
+const foodElement = document.getElementById('food');
+
+const yearElement = document.getElementById('#year');
+
+const imageElement = document.getElementById('profileImage');
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = '<strong>${fullName}</strong>';
+
+textContext.currentYear = '${currentYear}';
+
+imageElement.setAttribute('src', profilePicture);
+
+imageElement.setAttribute('alt', 'Profile Image of {fullName}');
+/* Step 5 - Array */
+
+let favFoods = ['Pasta', 'Steak','Asparagus','Burgers'];
+
+foodElement.innerHTML = '${favFoods}';
+
+let oneFavFood = 'Chicken';
+
+favFoods.push(oneFavFood);
+
+foodElement.innerHTML += '<br>${favFoods}';
+
+favFoods.shift();
+
+foodElement.innerHTML += '<br>${favFoods}';
+
+favFoods.pop();
+
+foodElement.innerHTML += '<br>${favFoods}';
+
+
+
